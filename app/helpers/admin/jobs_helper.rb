2e68cde07d6623e6eda5a919e008b252db7c6d4a
lbias <306508354@qq.com>
2017-07-04 13:33:48 +0800

    font-awesome-railis, helper admin::jobs, and view admin::jobs#index

--- app/helpers/admin/jobs_helper.rb
+++ app/helpers/admin/jobs_helper.rb
@@ -1,2 +1,22 @@
 module Admin::JobsHelper
+  # 判断显示/隐藏icon #
+  def render_job_status(job)
+    if job.is_hidden
+      "fa fa-lock"
+    else
+      "fa fa-globe"
+    end
+  end
+
+  # 薪水区间 #
+  def render_job_wage(job)
+    "#{job.wage_lower_bound} k ~ #{job.wage_upper_bound} k"
+  end
+
+  # 判断是否隐藏该职缺 #
+  def render_job_hidden(job)
+    if job.is_hidden
+      "hidden-box"
+    end
+  end
 end
