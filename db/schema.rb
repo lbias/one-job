359ac663a4dd7b1778343a02bd66d92b939f1fa7
lbias <306508354@qq.com>
2017-07-04 13:48:00 +0800

    add name, is_admin, is_website_admin to table users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170704042413) do
+ActiveRecord::Schema.define(version: 20170704054534) do
 
   create_table "jobs", force: :cascade do |t|
     t.integer "user_id"
@@ -40,6 +40,9 @@ ActiveRecord::Schema.define(version: 20170704042413) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.boolean "is_admin", default: false
+    t.boolean "is_website_admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
