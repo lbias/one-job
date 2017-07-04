d91300673e50a72feda76ff18de8a1b495425c50
lbias <306508354@qq.com>
2017-07-04 23:56:45 +0800

    db table loactions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170704091846) do
+ActiveRecord::Schema.define(version: 20170704154943) do
 
   create_table "collections", force: :cascade do |t|
     t.integer "user_id"
@@ -29,6 +29,14 @@ ActiveRecord::Schema.define(version: 20170704091846) do
     t.integer "wage_lower_bound"
     t.integer "wage_upper_bound"
     t.string "contact_mail"
+    t.boolean "is_hidden", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "locations", force: :cascade do |t|
+    t.string "name"
+    t.integer "sort"
     t.boolean "is_hidden", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
