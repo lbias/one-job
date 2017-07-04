8339d87fcf857a6342f2999fd04d8d45403f5c6a
lbias <306508354@qq.com>
2017-07-04 16:25:28 +0800

    record current user resume

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -6,9 +6,10 @@ class JobsController < ApplicationController
 
   def show
     @job = Job.find(params[:id])
+    @resumes = Resume.where(:job => @job, :user => current_user)
     if @job.is_hidden
       redirect_to root_path, alert: "此职缺暂未开放。"
     end
   end
-  
+
 end
