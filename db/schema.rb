e4c8a5f619054438ccb1ea8a6b51c1a7a66fe294
lbias <306508354@qq.com>
2017-07-04 13:01:51 +0800

    view admin::jobs#_form, new

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170702180025) do
+ActiveRecord::Schema.define(version: 20170704042413) do
+
+  create_table "jobs", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "name"
+    t.text "content"
+    t.string "company"
+    t.integer "category_id"
+    t.integer "location_id"
+    t.integer "wage_lower_bound"
+    t.integer "wage_upper_bound"
+    t.string "contact_mail"
+    t.boolean "is_hidden", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
