8a0e7b9a7ac898949bb8e988a552a9f8ec201b2b
lbias <306508354@qq.com>
2017-07-05 22:21:04 +0800

    paginate in controller account::collections#index

--- app/controllers/account/collections_controller.rb
+++ app/controllers/account/collections_controller.rb
@@ -2,7 +2,7 @@ class Account::CollectionsController < ApplicationController
 
   def index
     @user = current_user
-    @collections = Collection.where(:user => @user).order("created_at DESC")
+    @collections = Collection.where(:user => @user).order("created_at DESC").paginate(:page => params[:page], :per_page => 7)
   end
 
 end
