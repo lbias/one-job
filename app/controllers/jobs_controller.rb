3b4b86f9bd84c65e4da5b9f1d51fccf5676282b8
lbias <306508354@qq.com>
2017-07-05 23:36:45 +0800

    add job filter in controller jobs#index

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -4,7 +4,49 @@ class JobsController < ApplicationController
   def index
     @locations = Location.published.sortA
     @categorys = Category.published.sortA
-    @jobs = Job.published.recent.paginate(:page => params[:page], :per_page => 10)
+
+    # 目前只能单个条件筛选
+    # 判断是否筛选城市
+    if params[:location].present?
+      @location = params[:location]
+      @location_id = Location.find_by(name: @location)
+
+      if @location == '所有城市'
+        @jobs = Job.published.recent.paginate(:page => params[:page], :per_page => 10)
+      else
+        @jobs = Job.where(:location => @location_id).published.recent.paginate(:page => params[:page], :per_page => 10)
+      end
+
+    # 判断是否筛选职位类型
+    elsif params[:category].present?
+      @category = params[:category]
+      @category_id = Category.find_by(name: @category)
+
+      if @category == '所有类型'
+        @jobs = Job.published.recent.paginate(:page => params[:page], :per_page => 10)
+      else
+        @jobs = Job.where(:category => @category_id).published.recent.paginate(:page => params[:page], :per_page => 10)
+      end
+
+    # 判断是否筛选薪水
+    elsif params[:wage].present?
+      @wage = params[:wage]
+      if @wage == '5k以下'
+        @jobs = Job.wage1.published.recent.paginate(:page => params[:page], :per_page => 10)
+      elsif @wage == '5~10k'
+        @jobs = Job.wage2.published.recent.paginate(:page => params[:page], :per_page => 10)
+      elsif @wage == '10~15k'
+        @jobs = Job.wage3.published.recent.paginate(:page => params[:page], :per_page => 10)
+      elsif @wage == '15~25k'
+        @jobs = Job.wage4.published.recent.paginate(:page => params[:page], :per_page => 10)
+      else
+        @jobs = Job.wage5.published.recent.paginate(:page => params[:page], :per_page => 10)
+      end
+
+    # 预设显示所有公开职位
+    else
+      @jobs = Job.published.recent.paginate(:page => params[:page], :per_page => 10)
+    end
   end
 
   def show
