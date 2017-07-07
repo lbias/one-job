b889923ec9f90f94cefd95e199a67f019ff82451
lbias <306508354@qq.com>
2017-07-07 14:01:37 +0800

    change sqlites3 to pg

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170705122519) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.string "icon"
